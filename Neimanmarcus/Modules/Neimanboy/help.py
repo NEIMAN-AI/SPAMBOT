4ec5c78d946de5ec07b8df325ed0cae7e9a54cb3
NEIMAN-AI <136953008+NEIMAN-AI@users.noreply.github.com>
2023-06-19 20:18:49 +0530

    Create help.py

---
+++ Neimanmarcus/Modules/Neimanboy/help.py
@@ -0,0 +1,35 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, Message
+
+from Data import Data
+from LegendGirl.Config import *
+
+if HELP_MSG:
+    HELP_MSG = HELP_MSG
+else:
+    HELP_MSG = "[Spam Bot](https://t.me/TeamLegendXD) Help Menu"
+
+
+@Client.on_message(filters.command(["help"], prefixes=HANDLER))
+async def _help(Legend: Client, message: Message):
+    HELP_MSG = "Help Menu Powered By @TeamLegendXD"
+    if ".jpg" in HELP_PIC or ".png" in HELP_PIC:
+        await Legend.send_photo(
+            message.chat.id,
+            HELP_PIC,
+            caption=HELP_MSG,
+            reply_markup=InlineKeyboardMarkup(Data.HELP_MENU1),
+        )
+    elif ".mp4" in HELP_PIC.lower():
+        await Legend.send_video(
+            message.chat.id,
+            HELP_PIC,
+            caption=HELP_MSG,
+            reply_markup=InlineKeyboardMarkup(Data.HELP_MENU1),
+        )
+    else:
+        await Legend.send_message(
+            message.chat.id,
+            HELP_MSG,
+            reply_markup=InlineKeyboardMarkup(Data.HELP_MENU1),
+)
