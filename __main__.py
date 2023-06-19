01ad3ccd0084dc4acd708534afadc9cce506fc71
NEIMAN-AI <136953008+NEIMAN-AI@users.noreply.github.com>
2023-06-19 18:31:43 +0530

    Create __main__.py

---
+++ __main__.py
@@ -0,0 +1,11 @@
+import asyncio
+
+from .core.clients import *
+
+loop = asyncio.get_event_loop()
+
+if __name__ == "__main__":
+
+    loop.run_until_complete(Start_BotSpam())
+
+    print(" Good Bye ! ")
