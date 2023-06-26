003bfb4c5ce7c260384cdcb2995cc5629580d150
Neiman Marcus <136953008+NEIMAN-AI@users.noreply.github.com>
2023-06-26 19:17:40 +0530

    Create leave.py

---
+++ Neiman/Neimanboy/leave.py
@@ -0,0 +1,36 @@
+from config import L1, L2, L3, L4, L5, L6, L7, L8, L9, L10, SUDO_USERS, CMD_HNDLR as hl
+
+from telethon import events
+from telethon.tl.functions.channels import LeaveChannelRequest
+
+
+@L1.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+@L2.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+@L3.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+@L4.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+@L5.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+@L6.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+@L7.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+@L8.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+@L9.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+@L10.on(events.NewMessage(incoming=True, pattern=r"\%sleave(?: |$)(.*)" % hl))
+async def leave(e):
+    if e.sender_id in SUDO_USERS:
+
+        if len(e.text) > 7:
+            event = await e.reply("» ʟᴇᴀᴠɪɴɢ...")
+            mkl = e.text.split(" ", 1)
+            try:
+                await event.client(LeaveChannelRequest(int(mkl[1])))
+            except Exception as e:
+                await event.edit(str(e))
+        else:
+             if e.is_private:
+                  alt = f"**» ʏᴏᴜ ᴄᴀɴ'ᴛ ᴅᴏ ᴛʜɪꜱ ʜᴇʀᴇ !!**\n\n» {hl}leave <ᴄʜᴀɴɴᴇʟ/ᴄʜᴀᴛ ɪᴅ> \n» {hl}leave : ᴛʏᴘᴇ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ, ʙᴏᴛ ᴡɪʟʟ ᴀᴜᴛᴏ ʟᴇᴀᴠᴇ ᴛʜᴀᴛ ɢʀᴏᴜᴘ."
+                  await e.reply(alt)
+             else:
+                  event = await e.reply("» ʟᴇᴀᴠɪɴɢ...")
+                  try:
+                      await event.client(LeaveChannelRequest(int(e.chat_id)))
+                  except Exception as e:
+                      await event.edit(str(e)
